4af75d93275ec4e20d1f1ff48075ffac55deefa2
xinyue1007 <851910295@qq.com>
2021-06-03 14:16:23 +0800

    添加跑马灯及按键用例

---
+++ register_version/Marquee/OBJ/key.d
@@ -0,0 +1,15 @@
+..\obj\key.o: ..\HARDWARE\KEY\key.c
+..\obj\key.o: ..\HARDWARE\KEY\key.h
+..\obj\key.o: ..\..\SYSTEM\sys\sys.h
+..\obj\key.o: ..\..\SYSTEM\sys\stm32f4xx.h
+..\obj\key.o: ..\..\SYSTEM\sys\stm32f429xx.h
+..\obj\key.o: ..\..\SYSTEM\sys\core_cm4.h
+..\obj\key.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+..\obj\key.o: ..\..\SYSTEM\sys\core_cmInstr.h
+..\obj\key.o: ..\..\SYSTEM\sys\cmsis_armcc.h
+..\obj\key.o: ..\..\SYSTEM\sys\core_cmFunc.h
+..\obj\key.o: ..\..\SYSTEM\sys\core_cmSimd.h
+..\obj\key.o: ..\..\SYSTEM\sys\system_stm32f4xx.h
+..\obj\key.o: ..\..\SYSTEM\usart\usart.h
+..\obj\key.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+..\obj\key.o: ..\..\SYSTEM\delay\delay.h
